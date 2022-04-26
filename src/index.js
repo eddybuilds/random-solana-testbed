d3a62531d6b66916b5fc2931e8ba3b9470b7f0d6
eddybuilds <hey@eddybuilds.io>
2022-04-25 21:48:05 -0700

    Send some solana around :)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-console.log("Hello!");
+import {
+  clusterApiUrl,
+  Connection,
+  Keypair,
+  sendAndConfirmTransaction,
+  SystemProgram,
+  Transaction
+} from "@solana/web3.js";
+
+
+const alice = Keypair.generate();
+const bob = Keypair.generate();
+
+let connection = new Connection(clusterApiUrl('testnet'));
+
+const airdropSignature = await connection.requestAirdrop(
+  alice.publicKey,
+  1000000000,
+);
+
+await connection.confirmTransaction(airdropSignature);
+
+console.log(`Alice: ${(await connection.getBalance(alice.publicKey)) / 1000000000} SOL`);
+console.log(`Bob:   ${(await connection.getBalance(alice.publicKey)) / 1000000000} SOL`);
+
+let transaction = new Transaction();
+
+transaction.add(
+  SystemProgram.transfer({
+    fromPubkey: alice.publicKey,
+    toPubkey: bob.publicKey,
+    lamports: 100000000
+  })
+);
+
+await sendAndConfirmTransaction(
+  connection,
+  transaction,
+  [alice]
+);
+
+console.log(`Alice: ${(await connection.getBalance(alice.publicKey)) / 1000000000} SOL`);
+console.log(`Bob:   ${(await connection.getBalance(alice.publicKey)) / 1000000000} SOL`);
--- switchboard/generateFiles.js
+++
@@ -1,9 +0,0 @@
-import { SwitchboardTestEnvironment } from "@switchboard-xyz/switchboard-v2";
-import { Keypair, PublicKey } from "@solana/web3.js";
-
-const keyPair = Keypair.generate();
-
-const testEnv = await SwitchboardTestEnvironment.create(keyPair, {
-  USDC_MINT: new PublicKey("4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU"),
-});
-testEnv.writeAll("./switchboard_test_env", process.cwd());
